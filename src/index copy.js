1d6298bec51d1161264c80208f5b60d76c606f93
cristianoramon <cristianramon@yahoo.com.br>
2024-10-02 14:14:39 -0300

    Projeto 3

--- src/index copy.js
+++ src/index copy.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './templetes/Home/App';
 import reportWebVitals from './reportWebVitals';
-
+import { CounterContextProvider } from './contexts/CounterContextOld2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
- 
+   <CounterContextProvider>
           <App />
+    </CounterContextProvider> 
     
   </React.StrictMode>
 );
